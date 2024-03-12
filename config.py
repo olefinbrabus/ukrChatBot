5c96f4add9f24d83ae593c70edb2e606f93cee4e
Sasha Klymenko <sashklym@gmail.com>
2024-03-12 12:45:03 +0200

    Used env vars inside the app

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
 from dotenv import dotenv_values
 
 BOT_TOKEN = dotenv_values()["KEY"]
+MONGODB_USERNAME = dotenv_values()["MONGODB_USERNAME"]
+MONGODB_PASSWORD = dotenv_values()["MONGODB_PASSWORD"]
 
 SITE_URL = "https://ukr-mova.in.ua/"
 SITE_API_EXAMPLES = "https://ukr-mova.in.ua/api-new?route=examples"
 SITE_API_CATEGORIES = "https://ukr-mova.in.ua/api-new?route=categories"
 
-MONGO_CLIENT = "mongodb://localhost:27017/"
+MONGO_CLIENT = f"mongodb://{MONGODB_USERNAME}:{MONGODB_PASSWORD}@localhost:27017/"
 MONGO_DATABASE = "urkChatBot"
